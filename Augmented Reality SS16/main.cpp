c5fb91885d297479e7792c7f587b4b217e5ea0da
Vadym Strelchenko <bowlingforsoap@mail.ru>
2016-05-04 19:07:18 +0200

    Initial Commit

---
+++ Augmented Reality SS16/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Augmented Reality SS16
+//
+//  Created by Strelchenko Vadym on 5/4/16.
+//  Copyright © 2016 Strelchenko Vadym. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
